b7276e1c946cc60f4d934356cc3f9ad849bed751
Hallabassuny7 <hallabassuny@gmail.com>
2023-12-07 15:50:35 +0200

    User

---
+++ OOP-Project/OOP-Peoject/OOP-Project/src/User.java
@@ -0,0 +1,64 @@
+public class User {
+    package oop;
+
+    public class User {
+        private int ID;
+        private String FirstName;
+        private String LastName;
+        private String Username;
+        private String Password;
+        public User(){
+            super();
+        }
+
+
+        public  User(int ID,String FirstName,String LastName,String Username, String Password){
+            this.ID=ID;
+            this.FirstName=FirstName;
+            this.LastName=LastName;
+            this.Username=Username;
+            this.Password=Password;
+        }
+
+        public int getID() {
+            return ID;
+        }
+
+        public void setID(int ID) {
+            this.ID = ID;
+        }
+
+        public String getFirstName() {
+            return FirstName;
+        }
+
+        public void setFirstName(String firstName) {
+            FirstName = firstName;
+        }
+
+        public String getLastName() {
+            return LastName;
+        }
+
+        public void setLastName(String lastName) {
+            LastName = lastName;
+        }
+
+        public String getUsername() {
+            return Username;
+        }
+
+        public void setUsername(String username) {
+            Username = username;
+        }
+
+        public String getPassword() {
+            return Password;
+        }
+
+        public void setPassword(String password) {
+            Password = password;
+        }
+    }
+
+}
