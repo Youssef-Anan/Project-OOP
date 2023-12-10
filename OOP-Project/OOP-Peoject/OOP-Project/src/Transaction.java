83fc9f84e7e23b80eb8bcaf77f39f80d7ee929c6
Kareem <aqwsdfre357@gmail.com>
2023-12-10 22:23:59 +0200

    dsfc

--- OOP-Project/OOP-Peoject/OOP-Project/src/Transaction.java
+++ OOP-Project/OOP-Peoject/OOP-Project/src/Transaction.java
@@ -1,40 +1,61 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Transaction {
-    private int Id;
 
-    private Client Client;
+
+    ArrayList<Transaction> transactions = new ArrayList<>();
+    LocalDate date = LocalDate.now();
+    private Client client;
+    private Employee employee;
+    private float amount;
+    private Account srcAcc;
+
+    public Transaction(LocalDate date, Client client, Employee employee, float amount, Account srcAcc, Account dstAcc, ArrayList<Transaction> transactions) {
+        this.date = date;
+        this.client = client;
+        this.employee = employee;
+        this.amount = amount;
+        this.srcAcc = srcAcc;
+        this.transactions = transactions;
+    }
+
+    public static void createTransaction(LocalDate date, Client client, Employee employee, float amount, Account srcAcc, Account dstAcc, ArrayList<Transaction> transactions) {
+        Transaction trans = new Transaction(date, client, employee, amount, srcAcc, dstAcc, transactions);
+
+
+
+   /* private Client Client;
     private Employee Employee;
-     private String Date;
-
-    public String getDate() {
-        return Date;
-    }
-
-    public void setDate(String date) {
-        Date = date;
-    }
-
+    //private String Date;
     private float Amount;
-    private Account SrcAcc, DstAcc;
+    private Account SrcAcc,DstAcc;
 
-    Transaction(String Date, Client Client, Employee Employee, float Amount, Account SrcAcc, Account DstAcc) {
-        this.Date = Date;
-        this.Client = Client;
-        this.Employee = Employee;
-        this.Amount = Amount;
-        this.SrcAcc = SrcAcc;
-        this.DstAcc = DstAcc;
+    LocalDate Date=LocalDate.now();
 
+    Transaction(Client Client,Employee Employee,float Amount,Account SrcAcc, Account DstAcc){
+        //this.Date=Date;
+        this.Client=Client;
+        this.Employee=Employee;
+        this.Amount=Amount;
+        this.SrcAcc=SrcAcc;
+        this.DstAcc=DstAcc;
+    }*/
+
+
+        // public  static void createTransaction(LocalDate Date,Client Client,Employee Employee,float Amount,Account SrcAcc, Account DstAcc){
+        //    Transaction trans = new Transaction(Date, Client, Employee, Amount, SrcAcc);
+        //}
+
+
+        // Do something with the 'trans' object, if needed
+
+        // Assuming you want to perform some operation with the 'dstAcc', you can include it here
     }
 
-    public static void addTransaction(Account source, Account destination, ArrayList<Transaction> transactions) {
-        Transaction trans = new Transaction(Date, "Client","Employee","Amount","");
-    }
-
-    public void GetTransactionDetail(String Date, float Amount, String SrcAcc, String DstAcc, String Client) {
-        System.out.println("The transaction of The account named" + Client + " transaction is complete! \n  It was on " + Date + "\nThe Amount is " + Amount + "It was from the account: " + SrcAcc + "\nIt was sent to the account" + DstAcc);
-    }
+    // public void GetTransactionDetail(String Date,float Amount,String SrcAcc, String DstAcc ,String Client){
+    //System.out.println("The transaction of The account named"+Client+" transaction is complete! \n  It was on "+Date+"\nThe Amount is "+ Amount+ "It was from the account: "+SrcAcc+"\nIt was sent to the account"+DstAcc );
+    //  }
 
 
 }
