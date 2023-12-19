8bd1d89e2567546426832f24bbe651b3600df574
Karim-Deghidy <kimodeghidy2004@gmail.com>
2023-12-19 15:03:34 +0200

    Transactions-Karim

--- OOP-Project/OOP-Peoject/OOP-Project/src/Transaction.java
+++ OOP-Project/OOP-Peoject/OOP-Project/src/Transaction.java
@@ -9,6 +9,7 @@ public class Transaction {
     private Employee employee;
     private float amount;
     private Account srcAcc;
+    private Account dstAcc;
 
     public Transaction(LocalDateTime date, Client client, Employee employee, float amount, Account srcAcc, Account dstAcc) {
         this.date = date;
@@ -23,6 +24,51 @@ public class Transaction {
         transactions.add(trans);
     }
 
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public Account getSrcAcc() {
+        return srcAcc;
+    }
+
+    public void setSrcAcc(Account srcAcc) {
+        this.srcAcc = srcAcc;
+    }
+
+    public Account getDstAcc() {
+        return dstAcc;
+    }
+
+    public void setDstAcc(Account dstAcc) {
+        this.dstAcc = dstAcc;
+    }
+
+    public void displaytransaction(){
+        System.out.println("The transaction was in : " + date+ "to the client "+getClient()+ "of amount :"+getAmount()+" \nThe employee : "+getEmployee() );
+        System.out.println("The transaction was sent from : "+getSrcAcc()+"to : "+getDstAcc());
+
+}
 
 
 }
