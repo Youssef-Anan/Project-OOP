534be4e907f283e2a765d146610ea47cc98c2254
Karim-Deghidy <kimodeghidy2004@gmail.com>
2023-12-19 15:52:41 +0200

    Transactions-Karim

--- OOP-Project/OOP-Peoject/OOP-Project/src/CurrentAccount.java
+++ OOP-Project/OOP-Peoject/OOP-Project/src/CurrentAccount.java
@@ -3,21 +3,33 @@ public class CurrentAccount extends Account {
     private static final String ACCOUNT_TYPE = "Current";
 
 
-
     public CurrentAccount(){
         super(1);
     }
 
-    public void evaluateInterest() {
+    public CurrentAccount(double balance, double interestRate)
+    {
+        super(ACCOUNT_TYPE, balance);
+        this.interestRate = interestRate;
+    }
+
+
+    @Override
+    public void evaluateInterest()
+    {
         if (balance >= 3000) {
             interestRate = 0;
         } else {
             interestRate=0.12;
         }
     }
-    public void applyFees() {
-        balance -= balance * interestRate;
+    public void applyFees()
+    {
+        double interest = getBalance() * interestRate;
+        setBalance(getBalance() + interest);
+        System.out.println("Interest evaluated. New balance: " + getBalance());
     }
 
 
 }
+
--- OOP-Project/OOP-Peoject/OOP-Project/src/SavingsAccount.java
+++ OOP-Project/OOP-Peoject/OOP-Project/src/SavingsAccount.java
@@ -1,6 +1,6 @@
-import java.io.Serializable;
+import java.time.LocalDate;
 
-public class SavingsAccount extends Account implements Serializable {
+public class SavingsAccount extends Account  {
     private static final long serialVersionUID = 1L;
 
     private static final String ACCOUNT_TYPE = "Savings";
@@ -11,25 +11,28 @@ public class SavingsAccount extends Account implements Serializable {
 super(id);
     }
 
-    public SavingsAccount(double balance, double interestRate) {
+    public SavingsAccount(double balance, double interestRate)
+    {
         super(ACCOUNT_TYPE, balance);
         this.interestRate = interestRate;
     }
 
 
     @Override
-    public void evaluateInterest() {
+    public void evaluateInterest()
+    {
         double interest = getBalance() * interestRate;
         setBalance(getBalance() + interest);
         System.out.println("Interest evaluated. New balance: " + getBalance());
     }
 
     @Override
-    public void applyFees() {
+    public void applyFees()
+    {
         // Savings accounts do not have fees
     }
 
-    public static SavingsAccount createSavingsAccount(double initialBalance, double interestRate) {
+    public  SavingsAccount createSavingsAccount(double initialBalance, double interestRate) {
         SavingsAccount savingsAccount = new SavingsAccount(initialBalance, interestRate);
         savingsAccount.writeAccountToFile();
         return savingsAccount;
