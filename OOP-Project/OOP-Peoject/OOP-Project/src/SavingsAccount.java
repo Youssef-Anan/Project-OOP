2e05599256c5ec720d2fed0055717236688e13ba
yusefemad <yemoden@gmail.com>
2023-12-21 18:33:35 +0200

    yusef adding comments

--- OOP-Project/OOP-Peoject/OOP-Project/src/SavingsAccount.java
+++ OOP-Project/OOP-Peoject/OOP-Project/src/SavingsAccount.java
@@ -5,9 +5,12 @@ public class SavingsAccount extends Account  {
 
     public SavingsAccount(int id)
     {
+    // Call the constructor of the superclass (Account) with specific parameters
     super(id,"5484","saving account");
+    // Set the interest rate for this savings account
     this.DEFAULT_INTEREST_RATE = DEFAULT_INTEREST_RATE;
-        this.local_Date = LocalDate.now();
+    // Set the creation date of the account to the current date
+    this.local_Date = LocalDate.now();
     }
     public SavingsAccount() {
 
@@ -17,9 +20,12 @@ public class SavingsAccount extends Account  {
     @Override
     public void evaluateInterest() {
         LocalDate currentDate = LocalDate.now();
+        // Check if a year has passed since the last interest evaluation
         if (local_Date.plusYears(1).isBefore(currentDate) || local_Date.equals(currentDate)) {
+            // Calculate and add interest to the account balance
             double interest = getBalance() * DEFAULT_INTEREST_RATE;
             setBalance(getBalance() + interest);
+            // Update the last interest evaluation date
             local_Date = currentDate;
             System.out.println("Interest evaluated. New balance: " + getBalance());
         } else {
